db4c884f7f30b40377dd6995bd3b5d5727535aff
hyperoot <rajesh@hyperoot.dev>
2025-06-25 22:04:21 +0000

    Rename outputs for cloudflared role and secret IDs for clarity

--- terraform/vault/outputs.tf
+++ terraform/vault/outputs.tf
@@ -1,8 +1,8 @@
-output "role_id" {
+output "cloudflared_role_id" {
   value = vault_approle_auth_backend_role.cloudflared.role_id
 }
 
-output "secret_id" {
+output "cloudflared_secret_id" {
   value     = vault_approle_auth_backend_role_secret_id.cloudflared.secret_id
   sensitive = true
 }
